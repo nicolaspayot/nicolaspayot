b9a9b130f7d0f0a9cfc6a0844a3c60004e7e12b5
Nicolas Payot <nicolas.payot@gmail.com>
2019-06-27 13:54:08 +0200

    Remove unused CSS from tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
-const tailwind = require("tailwindcss")
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.js'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
 
 module.exports = () => ({
-  plugins: [tailwind("./tailwind.config.js")],
+  plugins: [
+    tailwind('./tailwind.config.js'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 })
